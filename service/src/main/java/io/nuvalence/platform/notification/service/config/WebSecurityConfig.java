8abe2f10d10de6d3c62fc319ce647d5528f0c609
Juan Suarez <juansuarez@NUVALENCE-MB39.local>
2023-07-13 14:32:29 -0500

    Small token auth provider fix

--- service/src/main/java/io/nuvalence/platform/notification/service/config/WebSecurityConfig.java
+++ service/src/main/java/io/nuvalence/platform/notification/service/config/WebSecurityConfig.java
@@ -109,7 +109,8 @@ public class WebSecurityConfig {
                                 new FirebaseAuthenticationProvider(gcpProjectId, NAMESPACE),
                                 new SelfSignedTokenAuthenticationProvider(
                                         selfSignIssuer,
-                                        RsaKeyUtility.getPublicKeyFromString(selfSignPublicKey))),
+                                        RsaKeyUtility.getPublicKeyFromString(selfSignPublicKey),
+                                        NAMESPACE)),
                         LoggingContextFilter.class)
                 .build();
     }
