2997a81719a8281482876fb1d092813d863a0327
Juan Suarez <juansuarez@NUVALENCE-MB39.local>
2023-08-16 14:03:12 -0500

    Added tests TwilioSmsProviderTest

--- service/src/test/java/io/nuvalence/platform/notification/service/service/TwilioSmsProviderTest.java
+++ service/src/test/java/io/nuvalence/platform/notification/service/service/TwilioSmsProviderTest.java
@@ -1,18 +1,20 @@
 package io.nuvalence.platform.notification.service.service;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-
+import com.twilio.exception.ApiConnectionException;
+import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
+import io.nuvalence.platform.notification.service.exception.UnprocessableNotificationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.MockedStatic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class TwilioSmsProviderTest {
 
@@ -66,5 +68,47 @@ public class TwilioSmsProviderTest {
         Assertions.assertEquals("to_number", calledToNumber);
         Assertions.assertEquals(twilioPhoneNumber, calledFromNumber);
         Assertions.assertEquals("test_message", calledMessage);
+    }
+
+    @Test
+    void testSendSms_ApiConnectionException() {
+        try (MockedStatic<Message> messageMock = mockStatic(Message.class)) {
+            messageMock.when(() -> Message.creator(any(PhoneNumber.class), any(PhoneNumber.class), anyString()))
+                    .thenReturn(messageCreator);
+
+            ApiConnectionException expectedException = new ApiConnectionException("Expected exception");
+            doThrow(expectedException).when(messageCreator).create();
+
+            ApiConnectionException actualException = assertThrows(ApiConnectionException.class, () -> smsProvider.sendSms("to_number", "test_message"));
+            assertEquals(expectedException, actualException);
+        }
+    }
+
+    @Test
+    void testSendSms_ApiException() {
+        try (MockedStatic<Message> messageMock = mockStatic(Message.class)) {
+            messageMock.when(() -> Message.creator(any(PhoneNumber.class), any(PhoneNumber.class), anyString()))
+                    .thenReturn(messageCreator);
+
+            ApiException expectedException = new ApiException("Expected exception");
+            doThrow(expectedException).when(messageCreator).create();
+
+            ApiException actualException = assertThrows(ApiException.class, () -> smsProvider.sendSms("to_number", "test_message"));
+            assertEquals(expectedException, actualException);
+        }
+    }
+
+    @Test
+    void testSendSms_UnexpectedException() {
+        try (MockedStatic<Message> messageMock = mockStatic(Message.class)) {
+            messageMock.when(() -> Message.creator(any(PhoneNumber.class), any(PhoneNumber.class), anyString()))
+                    .thenReturn(messageCreator);
+
+            RuntimeException expectedException = new RuntimeException("Expected exception");
+            doThrow(expectedException).when(messageCreator).create();
+
+            RuntimeException actualException = assertThrows(RuntimeException.class, () -> smsProvider.sendSms("to_number", "test_message"));
+            assertEquals(expectedException, actualException);
+        }
     }
 }
