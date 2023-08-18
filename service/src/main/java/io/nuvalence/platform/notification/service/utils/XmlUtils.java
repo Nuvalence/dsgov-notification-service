aab3be8ac7534dcba2c045348176fa2baf37d05c
Deibys Quintero <dquintero@nuvalence.io>
2023-08-18 12:43:52 -0500

    DSG-1398 : improve validation message

--- service/src/main/java/io/nuvalence/platform/notification/service/utils/XmlUtils.java
+++ service/src/main/java/io/nuvalence/platform/notification/service/utils/XmlUtils.java
@@ -1,5 +1,11 @@
 package io.nuvalence.platform.notification.service.utils;
 
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
 import java.io.IOException;
 import java.io.StringReader;
 
@@ -13,17 +19,17 @@ public class XmlUtils {
     private XmlUtils() {}
 
     /**
-    * <p>
-    * Verifies the string represents a valid XML file.
-    * And returns it minified for transfer convenience.
-    * </p>
-    * 
-    * @param xmlString the XML string to be parsed
-    * @return valid XML string in pretty format
-    * @throws JDOMException when errors occur in parsing
-    * @throws IOException   when an I/O error prevents a document from being fully
-    *                       parsed
-    */
+     * <p>
+     * Verifies the string represents a valid XML file.
+     * And returns it minified for transfer convenience.
+     * </p>
+     *
+     * @param xmlString the XML string to be parsed
+     * @return valid XML string in pretty format
+     * @throws JDOMException when errors occur in parsing
+     * @throws IOException   when an I/O error prevents a document from being fully
+     *                       parsed
+     */
     public static String xmlValidateAndMinify(String xmlString) throws JDOMException, IOException {
 
         SAXBuilder saxBuilder = new SAXBuilder();
@@ -37,16 +43,16 @@ public class XmlUtils {
     }
 
     /**
-    * <p>
-    * Verifies the string represents a valid XML file.
-    * And returns its representation as a Jdom2 document.
-    * </p>
-    * @param xmlString the XML string to be parsed
-    * @return valid XML Jdom2 Document
-    * @throws JDOMException when errors occur in parsing
-    * @throws IOException   when an I/O error prevents a document from being fully
-    *                       parsed
-    */
+     * <p>
+     * Verifies the string represents a valid XML file.
+     * And returns its representation as a Jdom2 document.
+     * </p>
+     * @param xmlString the XML string to be parsed
+     * @return valid XML Jdom2 Document
+     * @throws JDOMException when errors occur in parsing
+     * @throws IOException   when an I/O error prevents a document from being fully
+     *                       parsed
+     */
     public static Document getXmlDocument(String xmlString) throws JDOMException, IOException {
 
         SAXBuilder saxBuilder = new SAXBuilder();
@@ -54,7 +60,8 @@ public class XmlUtils {
         saxBuilder.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
 
         try (var reader = new StringReader(xmlString)) {
-            return saxBuilder.build(reader);
+            Document xmlDoc = saxBuilder.build(reader);
+            return xmlDoc;
         }
     }
 }
