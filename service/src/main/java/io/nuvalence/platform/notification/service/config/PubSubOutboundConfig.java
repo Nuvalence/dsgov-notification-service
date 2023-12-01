21c4c2897372fd030a7221790d0bed3dcf5c7d3e
Andres Calderon <acalderon@nuvalence.io>
2023-12-01 15:03:25 -0500

    DSG-1418 fix PR comments

--- service/src/main/java/io/nuvalence/platform/notification/service/config/PubSubOutboundConfig.java
+++ service/src/main/java/io/nuvalence/platform/notification/service/config/PubSubOutboundConfig.java
@@ -5,7 +5,6 @@ import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.MessagingGateway;
@@ -60,10 +59,6 @@ public class PubSubOutboundConfig {
      * @return Message Handler
      */
     @Bean
-    @ConditionalOnProperty(
-            value = "spring.cloud.gcp.pubsub.enabled",
-            havingValue = "false",
-            matchIfMissing = true)
     @ServiceActivator(inputChannel = OUTPUT_CHANNEL)
     public MessageHandler localMessageSender() {
         return message -> {
